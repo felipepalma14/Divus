727cf1c02f06ca86e4c9b0141ad6a30f086eded3
Felipe <felipepalma14@gmail.com>
2015-06-06 10:43:41 -0400

    Aula sobre Colletions - MAP

---
+++ AcademiaJava/src/br/com/divus/aj3/colletions/map/Disciplina.java
@@ -0,0 +1,77 @@
+package br.com.divus.aj3.colletions.map;
+
+public class Disciplina {
+	private Integer id;
+	private String disciplina;
+	
+	
+	
+	
+	public Disciplina(Integer id, String disciplina) {
+		super();
+		this.id = id;
+		this.disciplina = disciplina;
+	}
+	
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((disciplina == null) ? 0 : disciplina.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Disciplina other = (Disciplina) obj;
+		if (disciplina == null) {
+			if (other.disciplina != null)
+				return false;
+		} else if (!disciplina.equals(other.disciplina))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Disciplina [id=" + id + ", disciplina=" + disciplina + "]";
+	}
+	
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+	
+	
+	
+
+}
