da24ba7b8dd694dcf4ba71091d84ed734bda66d6
Felipe <felipepalma14@gmail.com>
2015-07-18 11:06:14 -0400

    criacao do pacote do projeto

---
+++ AcademiaJava/src/br/com/projecto/Cliente.java
@@ -0,0 +1,12 @@
+package br.com.projecto;
+
+public class Cliente extends Pessoa{
+	Long id;
+	
+	public Cliente(String nome,String cpf){
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+}
---
+++ AcademiaJava/src/br/com/projecto/Funcionario.java
@@ -0,0 +1,16 @@
+package br.com.projecto;
+
+public class Funcionario extends Pessoa{
+	Long id;
+	String matricula;
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	
+	
+	
+
+}
